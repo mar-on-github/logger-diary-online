3f4ad5391fbff11e6031d54be4f28228680da397
Mar <101558380+mar-on-github@users.noreply.github.com>
2022-05-21 22:27:23 +0200

    Hide the Windows PHP away on my linux pc so I can focus jeez

--- setenv.bat
+++ setenv.bat
@@ -1,4 +1,4 @@
-set newpat=%cd%\php
+set newpat=%cd%\.php
 For /F "Delims=" %%I In ('echo %PATH% ^| find /C /I "%newpat%"') Do set pathExists=%%I 2>Nul
 If %pathExists%==0 ( 
     setx /M PATH "%PATH%;%newpat%" 
