aa639ffd3b99b798c56a368e7a35fa54afffd8dd
Mar <101558380+mar-on-github@users.noreply.github.com>
2022-05-22 03:39:50 +0200

    Finish the basic principle.

--- logger/served/index.php
+++ logger/served/index.php
@@ -4,23 +4,26 @@
 // -------------------------------------
 // Functions
 
-function SaveSettings(mixed $Setting,string $Value) {
+function SaveSettings(mixed $Setting, string $Value)
+{
     if (!file_exists(GetSaveFolder() . "/config/")) {
         mkdir(GetSaveFolder() . "/config/", 0777, true);
     }
     $settingfiletobeopened = GetSaveFolder() . "/config/" . $Setting . ".txt";
     file_put_contents($settingfiletobeopened, $Value);
 }
-function RetrieveSettings($Setting) {
+function RetrieveSettings($Setting)
+{
     $Value = NULL;
     $settingfiletobeopened = GetSaveFolder() . "/config/" . $Setting . ".txt";
     if (file_exists($settingfiletobeopened)) {
-    $f = fopen($settingfiletobeopened, 'r');
-    $Value = fgets($f);
+        $f = fopen($settingfiletobeopened, 'r');
+        $Value = fgets($f);
     }
     return $Value;
 }
-function GetSaveFolder(){
+function GetSaveFolder()
+{
     $homefolder = getenv("HOME");
     $SaveFolder = $homefolder . "/.logger-diary";
     if (!file_exists($SaveFolder)) {
@@ -29,7 +32,8 @@ function GetSaveFolder(){
     return $SaveFolder;
 }
 
-function AddEntry(STRING $Entry) {
+function AddEntry(STRING $Entry)
+{
     $filename = GetSaveFolder() . "/loggies.log";
     $fileContent = file_get_contents($filename);
     $Input = date('Y-m-d H:i:s') . "->" . $Entry;
@@ -39,18 +43,19 @@ function AddEntry(STRING $Entry) {
 //  ------------------------------------   < todo: Themes should not be part of the first release, as they slurp up my attention from the base.
 // Setting themes.
 if (isset($_REQUEST['set_theme'])) {
-if ($_REQUEST['set_theme'] == "light") {
-    SaveSettings("set_theme", "light");
-    header("Location: ./");
+    if ($_REQUEST['set_theme'] == "light") {
+        SaveSettings("set_theme", "light");
+        header("Location: ./");
+    }
+    if ($_REQUEST['set_theme'] == "dark") {
+        SaveSettings("set_theme", "dark");
+        header("Location: ./");
+    }
+    if ($_REQUEST['set_theme'] == "taupe") {
+        SaveSettings("set_theme", "taupe");
+        header("Location: ./");
+    }
 }
-if ($_REQUEST['set_theme'] == "dark") {
-    SaveSettings("set_theme", "dark");
-    header("Location: ./");
-}
-if ($_REQUEST['set_theme'] == "taupe") {
-    SaveSettings("set_theme", "taupe");
-    header("Location: ./");
-}}
 // If no theme is set, and no theme is known, just fall back to taupe theme.
 if ((RetrieveSettings('set_theme')) === NULL) {
     SaveSettings("set_theme", "taupe");
@@ -78,7 +83,7 @@ if ($_SERVER['REQUEST_URI'] === '/style') {
 }
 // --------------------------------------
 if ($_SERVER['REQUEST_URI'] === '/add') {
-
+    AddEntry($_POST['new_entry']);
     header("Location: /");
 }
 ?>
@@ -101,7 +106,18 @@ if ($_SERVER['REQUEST_URI'] === '/add') {
             <input type="text" name="new_entry"><input type="submit" value="Write down">
         </form>
     </div>
+    <div class="readback">
+        <ul>
+        <?php
+            $filename = GetSaveFolder() . "/loggies.log";
+            $loggies = file($filename);
+            foreach ($loggies as $entry) {
+            echo "<li>" . $entry . "</i>";
+            }
+        ?>
+        </ul>
+    </div>
     <div class="infofooter">
-        <p><?php echo "Files are saved in: '" . GetSaveFolder() . "'."; ?> </p>
+        <p><?php echo "Files are saved in: '" . GetSaveFolder() . "'. Currently used theme: '" . RetrieveSettings('set_theme') . "'. Last update: <b>may 22th '22.</b>"; ?> </p>
     </div>
 </body>
