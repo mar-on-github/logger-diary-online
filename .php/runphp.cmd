e6a637227fb0983ed390115bb3a00a3d544c746c
Mar <just.mar.ok2813@gmail.com>
2022-06-01 22:53:39 +0200

    Streamlined it

--- .php/runphp.cmd
+++ .php/runphp.cmd
@@ -1,15 +1,16 @@
-echo opejf
-pause
-set phptorun=%~dp0php.exe %*
-echo Running: %phptorun%
-md %~dp0temp
+@echo off
+echo this file should close, but not with an error.
+if exist "%~dp0temp" rmdir /S /Q "%~dp0temp"
+md "%~dp0temp"
 (
+    echo echo This script should run hidden.
     echo cd %cd%
-    echo call %phptorun%
+    echo call %~dp0php.exe %*
 ) > "%~dp0temp\phptorun.cmd"
 (
 echo Set WshShell = CreateObject^("WScript.Shell"^) 
 echo WshShell.Run chr^(34^) ^& "%~dp0temp\phptorun.cmd" ^& Chr^(34^), 0
 echo Set WshShell = Nothing
 ) > "%~dp0temp\runphp.vbs"
-start wscript "%~dp0temp\runphp.vbs"
+echo running cscript...
+wscript "%~dp0temp\runphp.vbs"
